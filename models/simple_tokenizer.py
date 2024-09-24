909fb2909e429d12ff82c03c23ae433100e6476f
Gina Wu <gina9726@gmail.com>
2024-09-24 13:29:56 -0700

    Update simple_tokenizer.py
    Add automatic script for downloading `bpe_simple_vocab_16e6.txt`.

--- models/simple_tokenizer.py
+++ models/simple_tokenizer.py
@@ -63,6 +63,8 @@ class SimpleTokenizer(object):
     def __init__(self, bpe_path: str = default_bpe()):
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        if not os.path.isfile(bpe_path):
+            os.system(f"wget https://github.com/KaiyangZhou/CoOp/raw/refs/heads/main/clip/bpe_simple_vocab_16e6.txt.gz -O {bpe_path}")
         merges = gzip.open(bpe_path).read().decode("utf-8").split('\n')
         merges = merges[1:49152-256-2+1]
         merges = [tuple(merge.split()) for merge in merges]
